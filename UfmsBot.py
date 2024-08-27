899a57c8a21bcda05c62662f1fe65c2667557191
arthurnmachado <fuginceas@gmail.com>
2024-08-27 12:45:25 -0400

    Atualizando bot para incluir UCDB e UNIDERP

--- UfmsBot.py
+++ UfmsBot.py
@@ -1,11 +1,9 @@
-### Bot versão 2.0 (atualizada para o novo twitter (X))
-
 import tweepy
 import datetime
 import os
 import dotenv
 
-dotenv.load_dotenv() # Carrega as variáveis de ambiente
+dotenv.load_dotenv()  # Carrega as variáveis de ambiente
 
 access_token = os.environ['ACCESS_TOKEN']
 access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
@@ -17,27 +15,33 @@ client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_t
 auth = tweepy.OAuthHandler(api_key, api_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
 
-ferias = datetime.date(2024,12,8) # Coloque aqui a data do início das férias no modelo (ANO,MES,DIA)
-agora = datetime.date.today() # Computa o dia de hoje
+# Dicionário com as datas de férias (chave: instituição, valor: data)
+ferias_dict = {
+    "UFMS": datetime.date(2024, 12, 8),
+    "UCDB": datetime.date(2024, 12, 17),
+    "UNIDERP": datetime.date(2024, 12, 7),
+}
 
-fdias_horas = ferias - agora # Diferença entre os dias colocados anteriormente (Tempo para as ferias)
-fdias = fdias_horas.days  # Agora pegamos somente os dias
+agora = datetime.date.today()  # Computa o dia de hoje
 
-# Temos que separar o caso em que falta apenas 1 dia, para poder mudar "dias" para "dia" no tweet
+for instituicao, ferias in ferias_dict.items():
+    fdias_horas = ferias - agora  # Diferença entre os dias colocados anteriormente (Tempo para as ferias)
+    fdias = fdias_horas.days  # Agora pegamos somente os dias
 
-if agora > ferias:
-    quit()
+    # Verifica se a data já passou
+    if agora > ferias:
+        continue
 
-# Mensagem base
-if fdias == 0:
-    message = "ATENÇÃO: COMEÇARAM AS FÉRIAS DA UFMS!!!"
-elif fdias == 1:
-    message = "Falta 1 dia para as férias da UFMS!!!"
-else:
-    # Mensagem para múltiplos de 10 ou outros dias
-    if fdias % 10 == 0:
-        message = f"Faltam {fdias} dias para as férias da UFMS!!!"
+    # Mensagem base
+    if fdias == 0:
+        message = f"ATENÇÃO: COMEÇARAM AS FÉRIAS DA {instituicao}!!!"
+    elif fdias == 1:
+        message = f"Falta 1 dia para as férias da {instituicao}!!!"
     else:
-        message = f"Faltam {fdias} dias para as férias da UFMS"
+        # Mensagem para múltiplos de 10 ou outros dias
+        if (fdias % 10) == 0:
+            message = f"Faltam {fdias} dias para as férias da {instituicao}!!!"
+        else:
+            message = f"Faltam {fdias} dias para as férias da {instituicao}"
 
-client.create_tweet(text=message)
+    client.create_tweet(text=message)  # Cria o tweet
